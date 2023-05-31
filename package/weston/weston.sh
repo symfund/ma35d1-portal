b43ce620726235c8d0ec1bec9732405ad404e901
Arthur <arthur@nuvoton.com>
2023-05-31 12:00:15 +0800

    add lvgl package

---
+++ package/weston/weston.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if test -z "${XDG_RUNTIME_DIR}" ; then
+	export XDG_RUNTIME_DIR=/tmp/xdg
+	if ! test -d "${XDG_RUNTIME_DIR}" ; then
+		mkdir -p "${XDG_RUNTIME_DIR}"
+		chmod 0700 "${XDG_RUNTIME_DIR}"
+	fi
+fi
+
+weston_pid=$(pidof weston)
+
+if [[ -z "${weston_pid}" ]] ; then
+	weston --tty=1 --config=/etc/xdg/weston.ini &
+fi
+
+# weston 9.0.0, display_id=0
+# weston 10.0.0, display_id=1
+display_id=0
+
+export WAYLAND_DISPLAY=wayland-${display_id}
