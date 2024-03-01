3bd68bcd5d33dd301a5d2ff1b98332eba1231ccc
Jiang Tianwen <twjiang@nuvoton.com>
2024-03-01 07:31:06 -0500

    lvgl wayland widgets demo

---
+++ examples/wayland/lvgl/lvgl-wayland-widgets-demo/main.c
@@ -0,0 +1,49 @@
+#include <sys/time.h>
+#include "lvgl/lvgl.h"
+#include "lv_drivers/wayland/wayland.h"
+#include "lv_demo_widgets.h"
+
+int main(void)
+{
+    lv_disp_t *disp = NULL;
+    bool open = true;
+
+    /* intiate LVGL */
+    lv_init();
+
+    /* initiate Wayland for LVGL */
+    lv_wayland_init();
+
+    disp = lv_wayland_create_window(640, 360, "LVGL widgets demo with Wayland", NULL);
+
+    lv_demo_widgets();
+
+    while (open) {
+        lv_timer_handler();
+        usleep(5000);
+        open = lv_wayland_window_is_open(disp);
+    }
+
+    lv_wayland_deinit();
+
+    return 0;
+}
+
+/*Set in lv_conf.h as `LV_TICK_CUSTOM_SYS_TIME_EXPR`*/
+uint32_t custom_tick_get(void)
+{
+    static uint64_t start_ms = 0;
+    if(start_ms == 0) {
+        struct timeval tv_start;
+        gettimeofday(&tv_start, NULL);
+        start_ms = (tv_start.tv_sec * 1000000 + tv_start.tv_usec) / 1000;
+    }
+
+    struct timeval tv_now;
+    gettimeofday(&tv_now, NULL);
+    uint64_t now_ms;
+    now_ms = (tv_now.tv_sec * 1000000 + tv_now.tv_usec) / 1000;
+
+    uint32_t time_ms = now_ms - start_ms;
+    return time_ms;
+}
